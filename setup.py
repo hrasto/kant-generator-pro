0eaf58d47f790eca4749c874cf754ee0705ac28f
Rastislav Hronsky <rastislavhronsky@Rastislavs-MacBook-Pro.local>
2021-11-20 13:19:16 +0100

    added some projet files

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="kgp-hrasto",
+    version="0.0.1",
+    author="Rastislav Hronsky",
+    author_email="hronskyr@gmail.com",
+    description="Python3 port of the Kant Generator Pro",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hrasto/kgp",
+    project_urls={
+        "Bug Tracker": "https://github.com/hrasto/kgp/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
